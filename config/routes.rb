2d2bd5d5be02b0d5aff1ea4b21e1222b37fcdc45
Mai Morita <mai.nishio0315@gmail.com>
2019-08-08 19:45:07 +0900

    Fixed bugs of CSS of all pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  get '/users/mypage', to: 'users#mypage'
+  get '/users/sign_out', to: 'users#sign_out'
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'  
   }
   root to: 'products#index'
+  resources :purchase, only: :show
+
   resources :products
   resources :users
-  resources :purchase, only: :show
-  get '/users/mypage' => 'users#mypage'
-  get '/users/sign_out' => 'users#sign_out'
 end
