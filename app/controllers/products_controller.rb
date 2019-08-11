a9af99ab65fc2476ec15ebe306cceac089250830
sumirow <yumo1470@yahoo.co.jp>
2019-08-11 21:40:01 +0900

    title & price

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,15 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.order(id: :desc).limit(4)
   end
   
   def new
   end
   
   def show
+  end
+
+  def product_parmit
+    params.require(:product).parmit(:name, :price)
   end
 end
