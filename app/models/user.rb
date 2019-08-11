42fccf4ba22de4277be01b6ca31274ddea9e9bb9
Mai Morita <mai.nishio0315@gmail.com>
2019-08-11 16:10:17 +0900

    Updated User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :bought_plural, class_name: "Purchase", dependent: :destroy
+  has_many :sold_plural, class_name: "Purchase", dependent: :destroy
+  has_many :comments
+  has_many :products, through: :comments
+  has_one :address
+  has_one :card
+  has_many :purchases
 end
