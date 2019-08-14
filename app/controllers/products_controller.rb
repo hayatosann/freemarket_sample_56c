daaea096cf614a1751a403b7ed1d31ec8e7076c4
hayatosann <sasano.hayato@icloud.com>
2019-08-14 20:20:21 +0900

    set controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,5 +6,9 @@ class ProductsController < ApplicationController
   end
   
   def show
+    @product = Product.find(params[:id])
+    @same_category_products = Product.where(category_id: @product.category_id).where.not(id:@product.id).order('created_at DESC').limit(6)
+    @category=Category.all
   end
+
 end
