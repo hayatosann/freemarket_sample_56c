c7e1b399a8285d050b98159d23939fb73f4e25ac
tkori <tkori19+github@gmail.com>
2019-08-15 18:47:00 +0900

    Create validations

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,10 @@
 class Card < ApplicationRecord
   belongs_to :user
+
+  VALID_NUMBER_REGEX = /\A(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6011[0-9]{12}|3(?:0[0-5]|[68][0-9])[0-9]{11}|3[47][0-9]{13})\z/
+  validates :number, presence: true, format: { with: VALID_NUMBER_REGEX }
+  validates :expiration_date, presence: true
+  VALID_SC_REGEX = /\A\d{3,4}\z/
+  validates :security_code, presence: true, format: { with: VALID_SC_REGEX }
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,18 @@ class User < ApplicationRecord
   has_one :address
   has_one :card
   has_many :purchases
+  
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
+  validates :nickname, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
+  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+  validates :phone, presence: true, format: { with: VALID_PHONE_REGEX }
 end
