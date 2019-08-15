50c51634a97e652e78f82fabeb95d4504becda23
kenji-nishie <kenji.nishie@gmail.com>
2019-08-15 19:26:13 +0900

    change settings

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,8 @@
 class Image < ApplicationRecord
   belongs_to :product,optional: true
+<<<<<<< Updated upstream
   mount_uploader :image_path, ImageUploader
+=======
+  mount_uploader :images, ImageUploader
+>>>>>>> Stashed changes
 end
