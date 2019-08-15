38fa93d5a22d8c7ec63389e6ce3bc05640f3dd43
kenji-nishie <kenji.nishie@gmail.com>
2019-08-15 16:39:11 +0900

    remove rename migrationfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_103416) do
+ActiveRecord::Schema.define(version: 2019_08_13_034529) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postal_code", null: false
