ffbb59457ac47cec1bbd368ff150ec4934ec85b3
hayatosann <sasano.hayato@icloud.com>
2019-08-19 13:36:11 +0900

    import brand&category data with csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,96 +1,54 @@
+require "csv"
+#csvファイルの読み込み
 
-lady = Category.create(:name=>"レディース")
-man = Category.create(:name=>"メンズ")
-baby_kids = Category.create(:name=>"ベビー・キッズ")
-interia = Category.create(:name=>"インテリア・住まい・雑貨")
-kitchen = Category.create(:name=>"本・音楽・ゲーム")
-clock = Category.create(:name=>"時計")
-beauty = Category.create(:name=>"コスメ・香水・美容")
-televition = Category.create(:name=>"テレビゲーム")
-leasure = Category.create(:name=>"スポーツ・レジャー")
-phone = Category.create(:name=>"スマートフォン・携帯電話")
-bike = Category.create(:name=>"バイク")
-instrument = Category.create(:name=>"楽器")
-automobile_parts = Category.create(:name=>"自動車パーツ")
-food = Category.create(:name=>"食品")
-home_automobile = Category.create(:name=>"国内自動車本体")
-foreign_automobile = Category.create(:name=>"外国自動車本体")
+# csvファイル作成時にブランド一覧サインであるア~ンの一文字と数字の一文字は削除済み
+# name: row['name']  左側がデータベースカラム、右側がcsvファイルのカラム
 
-lady_tops = lady.children.create(:name=>"トップス")
-lady_jacket = lady.children.create(:name=>"ジャケット/アウター")
-lady_pants = lady.children.create(:name=>"パンツ")
-lady_skirt = lady.children.create(:name=>"スカート")
-lady_one_piece = lady.children.create(:name=>"ワンピース")
-lady_shoes = lady.children.create(:name=>"靴")
-lady_pajama = lady.children.create(:name=>"ルームウェア/パジャマ")
-lady_leg_wear = lady.children.create(:name=>"レッグウェア")
-lady_hat = lady.children.create(:name=>"帽子")
-lady_bag = lady.children.create(:name=>"バッグ")
-lady_accessory = lady.children.create(:name=>"アクセサリー")
-lady_hair_accessory = lady.children.create(:name=>"ヘアアクセサリー")
-lady_small_accessory = lady.children.create(:name=>"小物")
-lady_watch = lady.children.create(:name=>"時計")
-lady_wig = lady.children.create(:name=>"ウィッグ/エクステ")
-lady_swimsuit= lady.children.create(:name=>"浴衣/水着")
-lady_suit = lady.children.create(:name=>"スーツ/フォーマル/ドレス")
-lady_baby = lady.children.create(:name=>"マタニティ")
-lady_other = lady.children.create(:name=>"その他")
+# brand一覧
+CSV.foreach('db/brand.csv', headers: true) do |row|
+  Brand.create(
+    name: row['name'],
+  )
+end
 
-lady_tops.children.create([{:name=>"Tシャツ/カットソー(半袖/袖なし)"}, {:name=>"Tシャツ/カットソー(七分/長袖)"},{:name=>"シャツ/ブラウス(半袖/袖なし)"},{:name=>"シャツ/ブラウス(七分/長袖)"},{:name=>"ポロシャツ"},{:name=>"キャミソール"},{:name=>"タンクトップ"},{:name=>"ホルターネック"},{:name=>"ニット/セーター"},{:name=>"チュニック"},{:name=>"カーディガン/ボレロ"},{:name=>"アンサンブル"},{:name=>"ベスト/ジレ"},{:name=>"パーカー"},{:name=>"トレーナー/スウェット"},{:name=>"ベアトップ/チューブトップ"},{:name=>"ジャージ"},{:name=>"その他"}])
-lady_jacket.children.create([{:name=>"テーラードジャケット"}, {:name=>"ノーカラージャケット"}, {:name=>"Gジャン/デニムジャケット"},{:name=>"レザージャケット"},{:name=>"ダウンジャケット"},{:name=>"ライダースジャケット"},{:name=>"ミリタリージャケット"},{:name=>"ダウンベスト"},{:name=>"ジャンパー/ブルゾン"},{:name=>"ポンチョ"},{:name=>"ロングコート"},{:name=>"トレンチコート"},{:name=>"ダッフルコート"},{:name=>"ピーコート"},{:name=>"チェスターコート"},{:name=>"モッズコート"},{:name=>"スタジャン"},{:name=>"毛皮/ファーコート"},{:name=>"スプリングコート"},{:name=>"スカジャン"},{:name=>"その他"}])
-lady_pants.children.create([{:name=>"デニム/ジーンズ"}, {:name=>"ショートパンツ"}, {:name=>"カジュアルパンツ"},{:name=>"ハーフパンツ"},{:name=>"チノパン"},{:name=>"ワークパンツ/カーゴパンツ"},{:name=>"クロップドパンツ"},{:name=>"サロペット/オーバーオール"},{:name=>"オールインワン"},{:name=>"サルエルパンツ"},{:name=>"ガウチョパンツ"},{:name=>"その他"}])
-lady_skirt.children.create([{:name=>"ミニスカート"}, {:name=>"ひざ丈スカート"}, {:name=>"ロングスカート"},{:name=>"キュロット"},{:name=>"その他"}])
-lady_one_piece.children.create([{:name=>"ミニワンピース"}, {:name=>"ひざ丈ワンピース"}, {:name=>"ロングワンピース"},{:name=>"その他"}])
-lady_shoes.children.create([{:name=>"ハイヒール/パンプス"}, {:name=>"ブーツ"}, {:name=>"サンダル"},{:name=>"スニーカー"},{:name=>"ミュール"},{:name=>"モカシン"},{:name=>"ローファー/革靴"},{:name=>"フラットシューズ/バレエシューズ"},{:name=>"長靴/レインシューズ"},{:name=>"その他"}])
-lady_pajama.children.create([{:name=>"パジャマ"}, {:name=>"ルームウェア"}])
-lady_leg_wear.children.create([{:name=>"ソックス"}, {:name=>"スパッツ/レギンス"}, {:name=>"ストッキング/タイツ"},{:name=>"レッグウォーマー"},{:name=>"その他"}])
-lady_hat.children.create([{:name=>"ニットキャップ/ビーニー"}, {:name=>"ハット"}, {:name=>"ハンチング/ベレー帽"},{:name=>"キャップ"},{:name=>"キャスケット"},{:name=>"麦わら帽子"},{:name=>"その他"}])
-lady_bag.children.create([{:name=>"ハンドバッグ"}, {:name=>"トートバッグ"}, {:name=>"エコバッグ"},{:name=>"リュック/バックパック"},{:name=>"ボストンバッグ"},{:name=>"スポーツバッグ"},{:name=>"ショルダーバッグ"},{:name=>"クラッチバッグ"},{:name=>"ポーチ/バニティ"},{:name=>"ボディバッグ/ウェストバッグ"},{:name=>"マザーズバッグ"},{:name=>"メッセンジャーバッグ"},{:name=>"ビジネスバッグ"},{:name=>"旅行用バッグ/キャリーバッグ"},{:name=>"ショップ袋"},{:name=>"和装用バッグ"},{:name=>"かごバッグ"},{:name=>"その他"}])
-lady_accessory.children.create([{:name=>"ネックレス"}, {:name=>"ブレスレット"}, {:name=>"バングル/リストバンド"},{:name=>"リング"},{:name=>"ピアス(片耳用)"},{:name=>"ピアス(両耳用)"},{:name=>"イヤリング"},{:name=>"アンクレット"},{:name=>"ブローチ/コサージュ"},{:name=>"チャーム"},{:name=>"その他"}])
-lady_hair_accessory.children.create([{:name=>"ヘアゴム/シュシュ"}, {:name=>"ヘアゴム/カチューシャ"}, {:name=>"ヘアピン"},{:name=>"その他"}])
-lady_small_accessory.children.create([{:name=>"長財布"}, {:name=>"折財布"}, {:name=>"コインケース/小銭入れ"},{:name=>"名刺入れ/定期入れ"},{:name=>"キーケース"},{:name=>"キーホルダー"},{:name=>"手袋/アームカバー"},{:name=>"ハンカチ"},{:name=>"ベルト"},{:name=>"マフラー/ショール"},{:name=>"ストール/スヌード"},{:name=>"バンダナ/スカーフ"},{:name=>"ネックウォーマー"},{:name=>"サスペンダー"},{:name=>"サングラス/メガネ"},{:name=>"モバイルケース/カバー"},{:name=>"手帳"},{:name=>"イヤマフラー"},{:name=>"傘"},{:name=>"レインコート/ポンチョ"},{:name=>"ミラー"},{:name=>"タバコグッズ"},{:name=>"その他"}])
-lady_watch.children.create([{:name=>"腕時計(アナログ)"}, {:name=>"腕時計(デジタル)"}, {:name=>"ラバーベルト"},{:name=>"レザーベルト"},{:name=>"金属ベルト"},{:name=>"その他"}])
-lady_wig.children.create([{:name=>"前髪ウィッグ"}, {:name=>"ロングストレート"}, {:name=>"ロングカール"},{:name=>"ショートストレート"},{:name=>"ショートカール"},{:name=>"その他"}])
-lady_swimsuit.children.create([{:name=>"浴衣"}, {:name=>"着物"}, {:name=>"振袖"},{:name=>"長襦袢/半襦袢"},{:name=>"水着セパレート"},{:name=>"水着ワンピース"},{:name=>"水着スポーツ用"},{:name=>"その他"}])
-lady_suit.children.create([{:name=>"スカートスーツ上下"}, {:name=>"パンツスーツ上下"}, {:name=>"ドレス"},{:name=>"パーティーバッグ"},{:name=>"シューズ"},{:name=>"ウェディング"},{:name=>"その他"}])
-lady_baby.children.create([{:name=>"トップス"}, {:name=>"アウター"}, {:name=>"インナー"},{:name=>"ワンピース"},{:name=>"パンツ/スパッツ"},{:name=>"スカート"},{:name=>"パジャマ"},{:name=>"授乳服"},{:name=>"その他"}])
-lady_other.children.create([{:name=>"コスプレ"}, {:name=>"下着"}, {:name=>"その他"}])
+parent_element = []
+children_element = []
 
-man_tops = man.children.create(:name=>"トップス")
-man_jacket = man.children.create(:name=>"ジャケット/アウター")
-man_pants = man.children.create(:name=>"パンツ")
-man_shoes = man.children.create(:name=>"靴")
-man_bag = man.children.create(:name=>"バッグ")
-man_suit = man.children.create(:name=>"スーツ")
-man_hat = man.children.create(:name=>"帽子")
-man_accessory = man.children.create(:name=>"アクセサリー")
-man_small_accessory = man.children.create(:name=>"小物")
-man_watch = man.children.create(:name=>"時計")
-man_swimsuit= man.children.create(:name=>"水着")
-man_leg_wear = man.children.create(:name=>"レッグウェア")
-man_under_wear = man.children.create(:name=>"アンダーウェア")
-man_other = man.children.create(:name=>"その他")
+parent_count = 0
+children_count = 0
 
-man_tops.children.create([{:name=>"Tシャツ/カットソー(半袖/袖なし)"}, {:name=>"Tシャツ/カットソー(七分/長袖)"},{:name=>"シャツ"},{:name=>"ポロシャツ"},{:name=>"タンクトップ"},{:name=>"ニット/セーター"},{:name=>"パーカー"},{:name=>"カーディガン"},{:name=>"スウェット"},{:name=>"ジャージ"},{:name=>"ベスト"},{:name=>"その他"}])
-man_jacket.children.create([{:name=>"テーラードジャケット"}, {:name=>"ノーカラージャケット"}, {:name=>"Gジャン/デニムジャケット"},{:name=>"レザージャケット"},{:name=>"ダウンジャケット"},{:name=>"ライダースジャケット"},{:name=>"ミリタリージャケット"},{:name=>"ナイロンジャケット"},{:name=>"フライトジャケット"},{:name=>"ダッフルコート"},{:name=>"ピーコート"},{:name=>"ステンカラーコート"},{:name=>"トレンチコート"},{:name=>"モッズコート"},{:name=>"チェスターコート"},{:name=>"スタジャン"},{:name=>"スカジャン"},{:name=>"ブルゾン"},{:name=>"マウンテンパーカー"},{:name=>"ダウンベスト"},{:name=>"ポンチョ"},{:name=>"カバーオール"},{:name=>"その他"}])
-man_pants.children.create([{:name=>"スラックス"}, {:name=>"チノパン"}, {:name=>"ショートパンツ"},{:name=>"ペインターパンツ"},{:name=>"サルエルパンツ"},{:name=>"オーバーオール"},{:name=>"その他"}])
-man_shoes.children.create([{:name=>"スニーカー"}, {:name=>"サンダル"}, {:name=>"ブーツ"},{:name=>"モカシン"},{:name=>"ドレス/ビジネス"},{:name=>"長靴/レインシューズ"},{:name=>"デッキシューズ"},{:name=>"その他"}])
-man_bag.children.create([{:name=>"ショルダーバッグ"}, {:name=>"トートバッグ"}, {:name=>"リュック/バックパック"}, {:name=>"ウエストポーチ"}, {:name=>"ボディーバッグ"}, {:name=>"ドラムバッグ"}, {:name=>"ビジネスバッグ"}, {:name=>"トラベルバッグ"}, {:name=>"メッセンジャーバッグ"}, {:name=>"エコバッグ"},{:name=>"その他"}])
-man_suit.children.create([{:name=>"スーツジャケット"}, {:name=>"スーツベスト"}, {:name=>"スラックス"},{:name=>"セットアップ"},{:name=>"その他"}])
-man_hat.children.create([{:name=>"キャップ"}, {:name=>"ハット"}, {:name=>"ニットキャップ/ビーニー"}, {:name=>"ハンチング/ベレー帽"}, {:name=>"キャスケット"}, {:name=>"サンバイザー"}, {:name=>"その他"}])
-man_accessory.children.create([{:name=>"ネックレス"}, {:name=>"ブレスレット"}, {:name=>"バングル/リストバンド"},{:name=>"リング"},{:name=>"ピアス(片耳用)"},{:name=>"ピアス(両耳用)"},{:name=>"アンクレット"},{:name=>"その他"}])
-man_small_accessory.children.create([{:name=>"長財布"}, {:name=>"折り財布"}, {:name=>"マネークリップ"},{:name=>"コインケース/小銭入れ"},{:name=>"名刺入れ/定期入れ"},{:name=>"キーケース"},{:name=>"キーホルダー"},{:name=>"ネクタイ"},{:name=>"手袋"},{:name=>"ハンカチ"},{:name=>"ベルト"},{:name=>"マフラー"},{:name=>"ストール"},{:name=>"バンダナ"},{:name=>"ネックウォーマー"},{:name=>"サスペンダー"},{:name=>"ウォレットチェーン"}])
-man_watch.children.create([{:name=>"ハンドバッグ"}, {:name=>"トートバッグ"}, {:name=>"エコバッグ"},{:name=>"リュック/バックパック"},{:name=>"ボストンバッグ"},{:name=>"スポーツバッグ"},{:name=>"ショルダーバッグ"},{:name=>"クラッチバッグ"},{:name=>"ポーチ/バニティ"},{:name=>"ボディバッグ/ウェストバッグ"},{:name=>"マザーズバッグ"},{:name=>"メッセンジャーバッグ"},{:name=>"ビジネスバッグ"},{:name=>"旅行用バッグ/キャリーバッグ"},{:name=>"ショップ袋"},{:name=>"和装用バッグ"},{:name=>"かごバッグ"},{:name=>"その他"}])
-man_swimsuit.children.create([{:name=>"ネックレス"}, {:name=>"ブレスレット"}, {:name=>"バングル/リストバンド"},{:name=>"リング"},{:name=>"ピアス(片耳用)"},{:name=>"ピアス(両耳用)"},{:name=>"アンクレット"}])
-man_leg_wear.children.create([{:name=>"ヘアゴム/シュシュ"}, {:name=>"ヘアゴム/カチューシャ"}, {:name=>"ヘアピン"},{:name=>"その他"}])
-man_under_wear.children.create([{:name=>"長財布"}, {:name=>"折財布"}, {:name=>"コインケース/小銭入れ"},{:name=>"名刺入れ/定期入れ"},{:name=>"キーケース"},{:name=>"キーホルダー"},{:name=>"手袋/アームカバー"},{:name=>"ハンカチ"},{:name=>"ベルト"},{:name=>"マフラー/ショール"},{:name=>"ストール/スヌード"},{:name=>"バンダナ/スカーフ"},{:name=>"ネックウォーマー"},{:name=>"サスペンダー"},{:name=>"サングラス/メガネ"},{:name=>"モバイルケース/カバー"},{:name=>"手帳"},{:name=>"イヤマフラー"},{:name=>"傘"},{:name=>"レインコート/ポンチョ"},{:name=>"ミラー"},{:name=>"タバコグッズ"},{:name=>"その他"}])
-man_other.children.create([{:name=>"腕時計(アナログ)"}, {:name=>"腕時計(デジタル)"}, {:name=>"ラバーベルト"},{:name=>"レザーベルト"},{:name=>"金属ベルト"},{:name=>"その他"}])
+# 親カテゴリー
+parent_category = CSV.read("db/parent_category.csv")
+parent_category.each_with_index do |parent, index|
+  parent = Category.create(name: parent[0])
+  parent_element[index] = parent
+end
 
- brand = Brand.create(:name=>"アーカー")
- user = User.new(:email=>"tanaka.tarou@gmail.com", :password => 'hugahuga', :password_confirmation => 'hugahuga', :nickname => 'taro', :family_name => '田中', :first_name => '太朗', :family_name_kana =>'タナカ', :first_name_kana =>'タロウ',:birthday =>'1992/11/08',:phone =>'09012123434')
- user.save!
- 
- product = user.products.build(name: "test_product", detail: "お気に入り", size_id: 1, brand_id: 1, condition_id: 1, delivery_fee_id: 1, shipping_method_id: 1, prefecture_from_id: 1, shipping_days_id: 1, price: 300, category_id: 384)
- product.save!
+# 子カテゴリー
+children_category = CSV.read("db/child_category.csv")
+children_category.each_with_index do |child,index|
+  if child != []
+    parent_data = parent_element[parent_count]
+    children = parent_data.children.create(name: child[0]) 
+    children_element[index] = children
+  else
+    parent_count += 1
+  end
+end
 
- 
+children_element = children_element.compact
+# 配列からnilを消去
+
+# 孫カテゴリー
+grandchild_category = CSV.read("db/grandchild_category.csv")
+grandchild_category.each_with_index do |grandchild,index|
+  if grandchild != []
+    children_data = children_element[children_count]
+    children_data.children.create(name: grandchild[0])
+  else
+    children_count += 1
+  end
+end
+
+# 本番環境では
+# rake db:seed RAILS_ENV=production
