379e4d48ab8a0d1caf5a22edf702a52d9e30b880
kenji-nishie <kenji.nishie@gmail.com>
2019-08-22 13:39:12 +0900

    add prompt

--- app/assets/javascripts/product_new.js
+++ app/assets/javascripts/product_new.js
@@ -1,4 +1,18 @@
 $(function() {
+  //出品ページ用の入力フォームを非同期で追加
+  
+  // appendボタンを押したとき
+  $("#appendButton").on("click", function() {
+    $("#lists").append('<li class="list">追加されたリスト</li>');
+  });
+
+  // removeボタンを押したとき
+  $("#removeButton").on("click", function() {
+    $(".list").remove();
+  });
+
+
+  //出品終了後のモーダル表示用
   $(".exhibition_submit").on("click", function() {
     $(".modal").fadeIn().addClass('is-show is-animate');
   });
